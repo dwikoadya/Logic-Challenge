ae284618c5d4d26ccaaebf31ee3e0eb6970edacd
dwikoadya <dwikoadya@gmail.com>
2020-06-24 11:01:44 +0700

    sum arguments

---
+++ challenge1.js
@@ -0,0 +1,12 @@
+function sum() {
+    var z = 0;
+    for (let i = 0;i < arguments.length; i++) {
+        z += arguments[i];
+    }
+    console.log(z);
+}
+
+sum(1,2,7);
+sum(1,4);
+sum(11);
+sum(10,3,6,7,9)
